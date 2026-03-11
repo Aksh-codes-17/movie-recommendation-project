7dab1896f61e36c3ce9fbaa34b94e062c10d7a38
Akshaya <s.akshaya1752007@gmail.com>
2026-03-11 22:41:59 +0530

    Update movie_recommendation.py

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -1,22 +1,12 @@
 import pandas as pd
-
-# Load dataset
 data = pd.read_csv("movie_datasets.csv")
 
 print("🎬 MOVIE RECOMMENDATION SYSTEM")
 print("--------------------------------")
-
-# Show some movies
 print("\nAvailable Movies:\n")
 print(data[["title","release_date","vote_average","genres","popularity"]].head(10))
-
-# Ask user for genre
 genre = input("\nEnter a genre (Action, Comedy, Drama etc): ")
-
-# Filter movies by genre
 filtered_movies = data[data["genres"].str.contains(genre, case=False, na=False)]
-
-# Sort by rating and popularity
 recommended = filtered_movies.sort_values(by=["vote_average","popularity"], ascending=False)
 
 print("\n⭐ Top Recommended Movies:\n")
@@ -31,4 +21,5 @@ if len(recommended) > 0:
         "overview"
     ]].head(5))
 else:
+
     print("❌ No movies found for this genre.")
