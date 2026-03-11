f918a3816b2afb7cec90d7eddc63a4126d370646
Akshaya <s.akshaya1752007@gmail.com>
2026-03-11 22:26:36 +0530

    Add hover effect to button with scale transformation

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("button").addEventListener("mouseover", function(){
+this.style.transform = "scale(1.1)";
+})
+
+document.querySelector("button").addEventListener("mouseout", function(){
+this.style.transform = "scale(1)";
+})
