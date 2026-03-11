e17ee951285f97d33acf60869a7bc625a6e148d3
Akshaya <s.akshaya1752007@gmail.com>
2026-03-11 23:06:46 +0530

    Enhance movie recommendation system with user input
    Added user interaction and display of available movies.

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -1,23 +1,25 @@
 import pandas as pd
-
 data = pd.read_csv("movie_datasets.csv")
 
-def recommend_movies(genre):
+print("🎬 MOVIE RECOMMENDATION SYSTEM")
+print("--------------------------------")
+print("\nAvailable Movies:\n")
+print(data[["title","release_date","vote_average","genres","popularity"]].head(10))
+genre = input("\nEnter a genre (Action, Comedy, Drama etc): ")
+filtered_movies = data[data["genres"].str.contains(genre, case=False, na=False)]
+recommended = filtered_movies.sort_values(by=["vote_average","popularity"], ascending=False)
 
-    filtered_movies = data[data["genres"].str.contains(genre, case=False, na=False)]
+print("\n⭐ Top Recommended Movies:\n")
 
-    recommended = filtered_movies.sort_values(
-        by=["vote_average","popularity"], ascending=False
-    )
+if len(recommended) > 0:
+    print(recommended[[
+        "title",
+        "release_date",
+        "vote_average",
+        "vote_count",
+        "genres",
+        "overview"
+    ]].head(5))
+else:
+    print("❌ No movies found for this genre.")
 
-    if len(recommended) > 0:
-        return recommended[[
-            "title",
-            "release_date",
-            "vote_average",
-            "genres",
-            "overview"
-        ]].head(5).to_dict(orient="records")
-
-    else:
-        return []
